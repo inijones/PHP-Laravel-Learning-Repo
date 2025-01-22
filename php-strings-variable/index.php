a5dd54f461fbf13fc18b60a20f285eb401a195bd
Ini_Jones <74598414+inijones@users.noreply.github.com>
2025-01-22 10:47:55 +0100

    Added the String and Variable file

---
+++ php-strings-variable/index.php
@@ -0,0 +1,16 @@
+<?php
+	echo "Hello, Learner!";
+
+	echo "\nWe hope you enjoyed this lesson.";
+    
+  echo "\nAre you excited?" . "\nTo learn more?";
+    
+  $favorite_language = "PHP";
+
+  $message = "$favorite_language is by far our favorite language.";
+
+  $message .= " It's yours now too, right?";
+    
+  echo "\n" . $message . " Right?!";  
+
+  // =& is the logical and operator. It returns true if both operands are true.
