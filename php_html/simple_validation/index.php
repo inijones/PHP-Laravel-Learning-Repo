f78ce12c4c1f3501af3e626bf50d6051d6226f87
Ini_Jones <74598414+inijones@users.noreply.github.com>
2025-03-02 17:44:02 +0100

    Added Simple form validation

---
+++ php_html/simple_validation/index.php
@@ -0,0 +1,42 @@
+<?php
+// Define checkWord() here:
+  
+  function checkWord($input, $letter) {
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+      if(strtolower($input[0]) != $letter) {
+        return "* This word must start with the letter ${letter}!";
+      }
+    }
+    return "";
+  }
+  
+?>
+
+<h1>Time to Practice our ABCs</h1>
+<form method="post" action="">
+    Enter a word that starts with the letter "a":
+    <br>
+    <input type="text" name="a-word" id="a-word" value="<?= htmlspecialchars($_POST["a-word"] ?? ""); ?>">
+    <p class="error" id="a-error"><?= checkWord($_POST["a-word"] ?? "", "a"); ?></p>
+    <br>      
+      
+    Enter a word that starts with the letter "b":
+    <br>
+    <input type="text" id="b-word" name="b-word" value="<?= htmlspecialchars($_POST["b-word"] ?? ""); ?>">
+    <p class="error" id="b-error"><?= checkWord($_POST["b-word"] ?? "", "b"); ?></p>
+    <br>      
+      
+    Enter a word that starts with the letter "c":
+    <br>
+    <input type="text" id="c-word" name="c-word" value="<?= htmlspecialchars($_POST["c-word"] ?? ""); ?>">
+    <p class="error" id="c-error"><?= checkWord($_POST["c-word"] ?? "", "c"); ?></p>
+    <br>
+      
+    <input type="submit" value="Submit Words">
+</form>
+
+<div>
+    <h3>"a" is for: <?= $_POST["a-word"] ?? ""; ?></h3>
+    <h3>"b" is for: <?= $_POST["b-word"] ?? ""; ?></h3>
+    <h3>"c" is for: <?= $_POST["c-word"] ?? ""; ?></h3>    
+</div>
